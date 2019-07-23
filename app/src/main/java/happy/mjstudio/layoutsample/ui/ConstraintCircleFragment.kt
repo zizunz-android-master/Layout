fabd3c91f51bfb96966e9a059de3d3d6ebe28c6c
MJ Studio <mym0404@gmail.com>
2019-07-23 16:58:23 +0900

    done!

---
+++ app/src/main/java/happy/mjstudio/layoutsample/ui/ConstraintCircleFragment.kt
@@ -0,0 +1,15 @@
+package happy.mjstudio.layoutsample.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import happy.mjstudio.layoutsample.R
+
+class ConstraintCircleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_constraint_percent,container,false)
+        return v
+    }
+}
